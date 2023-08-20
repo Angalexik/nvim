fbc1a0570a76305e312222f77c90777b76f1137b
Angalexik <58977805+Angalexik@users.noreply.github.com>
2023-08-20 10:28:29 +0200

    Fix strange dashboard thing

--- lua/dashboard.lua
+++ lua/dashboard.lua
@@ -436,7 +436,7 @@ local function file_button(fn, sc, short_fn)
 	local file_button_el = button(sc, ico_txt .. short_fn, "<cmd>e " .. fn .. " <CR>")
 	local fn_start = short_fn:match(".*/")
 	if fn_start ~= nil then
-		table.insert(fb_hl, { "Comment", #ico_txt - 2, #fn_start + #ico_txt - 2 })
+		table.insert(fb_hl, { "Comment", #ico_txt - 2, #fn_start + #ico_txt })
 	end
 	file_button_el.opts.hl = fb_hl
 	return file_button_el
