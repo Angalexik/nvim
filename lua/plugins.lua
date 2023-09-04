b0591325b387943bf16f1f68eecfa37242be77c5
Angalexik <58977805+Angalexik@users.noreply.github.com>
2023-09-04 18:59:23 +0200

    Format plugins.lua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -115,29 +115,29 @@ require("lazy").setup({
 					return vim.tbl_contains({ "()", "[]", "{}" }, pair)
 				end),
 				Rule("( ", " )")
-						:with_pair(function()
-							return false
-						end)
-						:with_move(function(opts)
-							return opts.prev_char:match(".%)") ~= nil
-						end)
-						:use_key(")"),
+					:with_pair(function()
+						return false
+					end)
+					:with_move(function(opts)
+						return opts.prev_char:match(".%)") ~= nil
+					end)
+					:use_key(")"),
 				Rule("{ ", " }")
-						:with_pair(function()
-							return false
-						end)
-						:with_move(function(opts)
-							return opts.prev_char:match(".%}") ~= nil
-						end)
-						:use_key("}"),
+					:with_pair(function()
+						return false
+					end)
+					:with_move(function(opts)
+						return opts.prev_char:match(".%}") ~= nil
+					end)
+					:use_key("}"),
 				Rule("[ ", " ]")
-						:with_pair(function()
-							return false
-						end)
-						:with_move(function(opts)
-							return opts.prev_char:match(".%]") ~= nil
-						end)
-						:use_key("]"),
+					:with_pair(function()
+						return false
+					end)
+					:with_move(function(opts)
+						return opts.prev_char:match(".%]") ~= nil
+					end)
+					:use_key("]"),
 			})
 
 			pairs.get_rule('"')[1]:with_pair(function()
@@ -418,11 +418,11 @@ require("lazy").setup({
 			},
 			-- you can enable a preset for easier configuration
 			presets = {
-				bottom_search = true,     -- use a classic bottom cmdline for search
-				command_palette = true,   -- position the cmdline and popupmenu together
+				bottom_search = true, -- use a classic bottom cmdline for search
+				command_palette = true, -- position the cmdline and popupmenu together
 				long_message_to_split = true, -- long messages will be sent to a split
-				inc_rename = false,       -- enables an input dialog for inc-rename.nvim
-				lsp_doc_border = false,   -- add a border to hover docs and signature help
+				inc_rename = false, -- enables an input dialog for inc-rename.nvim
+				lsp_doc_border = false, -- add a border to hover docs and signature help
 			},
 		},
 	},
