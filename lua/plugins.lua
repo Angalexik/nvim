e42796d013e044082cea475694de5c0776128a47
Angalexik <58977805+Angalexik@users.noreply.github.com>
2025-11-30 20:19:30 +0100

    fix treesitter being a little baby bitch

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -434,6 +434,7 @@ require("lazy").setup({
 		config = function()
 			require("nvim-treesitter.configs").setup({
 				ensure_installed = "all",
+				ignore_install = { "ipkg" },
 				highlight = {
 					enable = true,
 					disable = { "c_sharp", "dockerfile" },
